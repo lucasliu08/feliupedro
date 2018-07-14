9e3657833e262269875918ec3c807d3bd52c6021
lucasliu08 <40473190+lucasliu08@users.noreply.github.com>
2018-07-13 21:56:24 -0300

    Update lista.c

--- lista.c
+++ lista.c
@@ -1,13 +1,7 @@
-#include <stdio.h>
+include <stdio.h>
 #include <stdlib.h>
 
-/*
-lista de adjacencia
-X cria grafo lendo de arquivo
-X adiciona/remove um vértice
-X adiciona/remove uma nova aresta
-X retorna uma lista de nós vizinhos de um dado vértice
-*/
+/* lista de adjacencia */
 
 struct vizinhos
 {
@@ -21,6 +15,14 @@ struct vertice
     int valor;
     struct vertice* prox;
     struct vizinhos* listavizinhos;
+};
+
+struct ArestaRotulada
+{
+  int explorada;
+  int descoberta;
+  int v1;
+  int v2;
 };
 
 struct vizinhos* criaVizinho(int);
@@ -38,42 +40,90 @@ void AdicionaAresta(struct vertice**, int, int);
 void ImprimeVizinhos(struct vizinhos*);
 void ImprimeGrafo(struct vertice*);
 
+void BuscaGrafo(int, int**, struct ArestaRotulada**, int);
+void BuscaCompleta(int**, struct ArestaRotulada**, int, int);
+int EhConexo(int, int**, struct ArestaRotulada**, int, int);
+int TemCiclo(int**, struct ArestaRotulada**, int, int);
+int EhFloresta(int**, struct ArestaRotulada**, int, int);
+int EhArvore(int, int**, struct ArestaRotulada**, int, int);
+void ObterFlorestaGeradora(int**, struct ArestaRotulada**, struct vertice**, int, int);
+
 
 int main()
 {
-    //inicializa bando de gente
+    //ListaVertice = no cabeca da lista encadeada
+    //VerticesVisitados = vetor onde cada casa representa seu respectivo
+    //vertice e seu valor (1 ou 0) representa se esse vertice foi visitado ou nao
+    
+    //ListaArestas = lista de arestas, seus vertices e se essas foram exploradas
+    // e/ou descobertas (definida pelo struct ArestaRotulada acima da main)
+
     FILE* Arquivo = NULL; struct vertice* ListaVertice = NULL;
+    struct vertice* FlorestaGeradora = NULL;
     int VerticesLidos = 0; int arestas = 0; int i, j, v1, v2;
-    int VerticesAdicionados = 0; struct vertice* TempVert;
+    int VerticesAdicionados = 0; int VerticesAdiFlor = 0; struct vertice* TempVert;
+    int* VerticesVisitados = NULL; struct ArestaRotulada* ListaArestas = NULL;
     ////////////////////////////////////////////////////
 
 
     //abre arquivo, le N e M
     Arquivo = fopen ("grafo1.txt","r");
     fscanf(Arquivo, "%d\t%d", &VerticesLidos , &arestas);
-    ///////////////////////////////////////////////
+    ////////////////////////////////////////////////////
+
+
+    VerticesVisitados = (int*) malloc(sizeof(int) * VerticesLidos);
+    ListaArestas = (struct ArestaRotulada*) malloc(sizeof(struct ArestaRotulada) * arestas);
+
 
     for(i = 0; i < VerticesLidos; i++)
     {
       InsereVerticeFim(&ListaVertice, i, &VerticesAdicionados);
+      InsereVerticeFim(&FlorestaGeradora, i, &VerticesAdiFlor);
+      VerticesVisitados[i] = 0;
     }
 
     for(i = 0; i < arestas; i++)
     {
       fscanf(Arquivo, "%d\t%d", &v1, &v2);
       AdicionaAresta(&ListaVertice, v1, v2);
+      ListaArestas[i].explorada = 0;
+      ListaArestas[i].descoberta = 0;
+      ListaArestas[i].v1 = v1;
+      ListaArestas[i].v2 = v2;
     }
 
+    
 
-    ExcluiVertice(&ListaVertice, 3, &VerticesAdicionados);
-    printf("Excluído o vértice 3\n\n");
 
-    ExcluiAresta(&ListaVertice, 0, 4);
-    printf("Excluída a aresta 0, 4\n\n");
 
-    ImprimeGrafo(ListaVertice);
+    //BuscaGrafo(0, &VerticesVisitados, &ListaArestas, arestas);
 
+    //BuscaCompleta(&VerticesVisitados, &ListaArestas, arestas, VerticesLidos);
+
+
+    //ObterFlorestaGeradora(&VerticesVisitados, &ListaArestas, &FlorestaGeradora, arestas, VerticesLidos);
+
+    //EhConexo(0, &VerticesVisitados, &ListaArestas, VerticesLidos, arestas); 
+    
+    //TemCiclo(&VerticesVisitados, &ListaArestas, VerticesLidos, arestas);
+    
+    
+    //EhFloresta(&VerticesVisitados, &ListaArestas, VerticesLidos, arestas);
+    
+    
+    //EhArvore(0, &VerticesVisitados, &ListaArestas, VerticesLidos, arestas);
+
+
+
+
+
+
+
+    free(ListaArestas);
+    free(VerticesVisitados);
     LimpaListaVertice(&ListaVertice);
+    LimpaListaVertice(&FlorestaGeradora);
     fclose(Arquivo);
     return 0;
 }
@@ -287,5 +337,153 @@ void ImprimeGrafo(struct vertice* p)
     ImprimeVizinhos(p->listavizinhos);
     printf("\n\n");
     ImprimeGrafo(p->prox);
+  }
+}
+
+
+
+void BuscaGrafo(int r, int** V, struct ArestaRotulada** E, int m)
+{
+  (*V)[r] = 1; int i;
+  
+  for(i = 0; i < m; i++)
+  {
+    if((*E)[i].explorada == 0 && (*V)[(*E)[i].v1] == 1)
+    {
+      (*E)[i].explorada = 1;
+
+      if( (*V)[(*E)[i].v2] == 0 )
+      {
+        (*V)[(*E)[i].v2] = 1;
+        
+        (*E)[i].descoberta = 1;
+      }
+    }
+
+    if((*E)[i].explorada == 0 && (*V)[(*E)[i].v2] == 1)
+    {
+      (*E)[i].explorada = 1;
+      
+      if( (*V)[(*E)[i].v1] == 0 )
+      {
+        (*V)[(*E)[i].v1] = 1;
+        
+        (*E)[i].descoberta = 1;
+      }
+    }
+  }
+}
+
+
+
+void BuscaCompleta(int** V, struct ArestaRotulada** E, int m, int n)
+{
+  int i;
+
+  for(i = 0; i < m; i++)
+  {
+    (*E)[i].descoberta = 0;
+    (*E)[i].explorada = 0;
+  }
+
+  for(i = 0; i < n; i++)
+  {
+    (*V)[i] = 0;
+  }
+
+  for(i = 0; i < n; i++)
+  {
+    if((*V)[i] == 0)
+    {
+      BuscaGrafo(i, V, E, m);
+    }
+  }
+}
+
+
+int EhConexo(int r, int** V, struct ArestaRotulada** E, int n, int m)
+{
+  BuscaGrafo(r, V, E, m);
+  
+  int i;
+  for(i = 0; i < n; i++)
+  {
+    if ((*V)[i] == 0)
+    {
+      return 0;
+    }
+  }
+
+  return 1;
+}
+
+
+int TemCiclo(int** V, struct ArestaRotulada** E, int n, int m)
+{
+  BuscaCompleta(V, E, m, n);
+
+  int i;
+  for(i = 0; i < m; i++)
+  {
+    if((*E)[i].descoberta == 0)
+    {
+      return 1;
+    }
+  }
+
+  return 0;
+}
+
+
+int EhFloresta(int** V, struct ArestaRotulada** E, int n, int m)
+{
+  if(TemCiclo(V, E, n, m)  == 0)
+  {
+    return 1;
+  }
+
+  else
+  {
+    return 0;
+  }
+
+}
+
+
+int EhArvore(int r, int** V, struct ArestaRotulada** E, int n, int m)
+{
+  BuscaGrafo(r, V, E, m);
+
+  int i;
+  for(i=0; i < n; i++)
+  {
+    if((*V)[i] == 0)
+    {
+      return 0;
+    }
+  }
+  
+  for(i = 0; i < m; i++)
+  {
+    if((*E)[i].descoberta == 0)
+    {
+      return 0;
+    }
+  }
+
+  return 1;
+}
+
+
+void ObterFlorestaGeradora(int** V, struct ArestaRotulada** E, struct vertice** T, int m, int n)
+{
+  BuscaCompleta(V, E, m, n);
+  int i; int j = 0;
+  for(i = 0; i < m; i++)
+  {
+    if((*E)[i].descoberta == 1)
+    {
+      AdicionaAresta( T, (*E)[i].v1, (*E)[i].v2 );
+    }
   }
 }
